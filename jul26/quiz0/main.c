7e4e0e2cd5445b8a3ea85f74c6cb97c07202fa22
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-07-26 21:00:51 +0800

    jul26-quiz0: rewrite test pattern
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- jul26/quiz0/main.c
+++ jul26/quiz0/main.c
@@ -10,25 +10,23 @@ int is_aligned (const uint8_t *p, uint8_t byte_count)
 
 int main(int argc, char *argv[])
 {
-        /* contents: (address, byte_count) */
-        int arr[] = {
-                /* is_aligned */
-                0x00,0x01, 0x00,0x02, 0x00,0x04, 0x00,0x08,
-                0x00,0x10, 0x00,0x20, 0x00,0x40, 0x00,0x80,
+        void* data[] = {
+                (void *) 0,          /*  is_aligned()  */
+                (void *) 0xABCD7FFF, /* !is_aligned() */
+                (void *) 0xABCD8000, /*  is_aligned()  */
+        };
 
-                /* ! is_aligned */
-                0x01,0x02, 0x03,0x04, 0x07,0x08, 0x0F,0x10,
-                0x1F,0x20, 0x3F,0x40, 0x7F,0x80, 0x02,0x02,
-
-                /* is_aligned */
-                0x04,0x04, 0x08,0x08, 0x10,0x10, 0x20,0x20,
-                0x40,0x40, 0x80,0x80,
+        int pattern[] = {
+                0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80
         };
 
         printf("\n------ Start is_aligned test ----- \n\n");
 
-        for(int i = 0; i < ARRAY_SIZE(arr); i += 2) {
-                printf("is_aligned(0x%x, 0x%x) = %d \n", arr[i], arr[i + 1], is_aligned(arr[i], arr[i + 1]));
+        for(int i = 0; i < ARRAY_SIZE(data); i++) {
+                for (int j = 0; j < ARRAY_SIZE(pattern); j++) {
+                        printf("is_aligned(0x%02x, 0x%02x) = %d \n", data[i], pattern[j], is_aligned(data[i], pattern[j]));
+                }
+                printf("\n");
         }
         printf("\n");
 
